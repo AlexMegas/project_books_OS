6ec746b8c46f660119a74725937c5798298c3d32
Oleksandr <solovei.oleksandr@gmail.com>
2020-03-17 14:41:57 +0200

    added get patch delete

--- controllers/book.js
+++ controllers/book.js
@@ -1,9 +1,62 @@
 const Book = require('../models/bookSchema.js');
 
-exports.getBooks = (req, res) => {
-    Book.find({})
+
+// exports.deleteBookById = (req, res) => {
+//     const book = new Book(req.id);
+//     book.findOneAndDelete(id)     // find book by id and delete
+//         .then(result => {
+//             res.json({
+//                 book: result
+//             });
+//         });
+// }
+
+
+// exports.upddateBookById = (req, res) => {
+//     const book = new Book(req.id);
+//     book.findOneAndUpdate({"_id": id}, (req.body)=> {})     // update book parametres by id = findByIdAndUpdate
+//         .then(result => {
+//             res.json({
+//                 book: result
+//             });
+//         });
+// }
+
+exports.getBookByAuthor = (req, res) => {
+    Book.findOne(req.author)    // find book by Author  { "author": author }
         .then(book => {
-            throw new Error('my error:-)');
+            res.json({
+                book
+            })
+        })
+        .catch(
+            err => {
+                console.log(err)
+                res.status(404).send(err.message)
+            }
+        );
+};
+
+exports.getBookById = (req, res) => {
+    Book.findById(req.id) // find book by id, maybe to use callbak function ?
+        .then(book => {
+            res.json({
+                book
+            })
+        })
+        .catch(
+            err => {
+                console.log(err)
+                res.status(404).send(err.message)
+            }
+        );
+};
+
+
+exports.getBooks = (req, res) => {
+    Book.find({})   // find all books in the book store
+        .then(book => {
+            //throw new Error('my error :-(');
             res.json({
                 book
             })
@@ -18,7 +71,7 @@ exports.getBooks = (req, res) => {
 
 exports.addBook = (req, res) => {
     const book = new Book(req.body);
-    book.save()
+    book.save()     // save new book to the book store
         .then(result => {
             res.json({
                 book: result
--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const { getBooks, addBook } = require('../controllers/book.js');
+const { addBook, getBooks, getBookById, getBookByAuthor/*, updateBookById, deleteBookById*/ } = require('../controllers/book.js');
 
 const book = express.Router();
 
-book.post('/books', addBook);
-book.get('/books', getBooks);
+book.post('/books', addBook); // record new book
+book.get('/books', getBooks); // select all books
+book.get('/books/id:', getBookById); // select one book by its id
+book.get('/books/?author=', getBookByAuthor); // select book by author
+//book.patch('/books/id:', updateBookById); // update book parametres by id
+//book.delete('books/:', deleteBookById); // delete book by id
+
+
+// const id = find({ name: 'name'}).then(res => {return res});
+// router.get("/", #{id});
+
 
 module.exports = book;
