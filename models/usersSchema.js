338ca846a29b52c9cbcbe3c2d130b488bade1512
Oleksandr S <solovei.oleksandr@gmail.com>
2020-03-19 19:25:04 +0200

    added schema into models/userrSchema.js

---
+++ models/usersSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        dropDups: true
+    },
+    passw: {
+        type: String,
+        required: true
+    },
+    hash: String,
+    salt: String,
+});
+
+module.exports = mongoose.model("user", userSchema);
