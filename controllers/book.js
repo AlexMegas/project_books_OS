3cde75631310b7d459df949b57baadd7b2711951
Oleksandr <solovei.oleksandr@gmail.com>
2020-03-19 16:48:33 +0200

    updateBookById, deleteBookById : using Promises

--- controllers/book.js
+++ controllers/book.js
@@ -12,7 +12,6 @@ exports.addBook = (req, res) => {
 };
 
 
-
 exports.getBookById = (req, res) => {
     const bookId = req.params.id; //req.params: directly access the parsed route parameters from the path
     Book.findById(bookId) // find book by id
@@ -28,7 +27,6 @@ exports.getBookById = (req, res) => {
             }
         );
 };
-
 
 
 exports.getBookByQuery = (req, res) => {
@@ -65,34 +63,24 @@ exports.getBookByQuery = (req, res) => {
 };
 
 
-
 exports.updateBookById = (req, res) => {
     const bookId = req.params.id;
-    const promise = new Promise(function (resolve, reject) {
-        Book.findByIdAndUpdate(bookId, req.body, (error, result) => { // update book parametres by id = findByIdAndUpdate
-            if (error) reject();
-            resolve(result);
-        })
-    })
-    promise.then(res.send(`Record with id:${bookId} --> updated successfully!`));
-    promise.catch(
-        err => {
-            console.log(err)
-            res.status(404).send(err.message)
-        });
-}
-
+    Book.findByIdAndUpdate(bookId, req.body)
+        .then(res.send(`Record with id:${bookId} --> updated successfully!`))
+        .catch(
+            err => {
+                console.log(err)
+                res.status(404).send(err.message)
+            });
+};
 
 
 exports.deleteBookById = (req, res) => {
-    const bookId = req.params.id;
-    Book.findOneAndDelete({ // find book by id and delete
-        _id: bookId
-    }, function (err) {
-        if (err) {
+    const bookId = req.params.id;// find book by id and delete
+    Book.findOneAndDelete({ _id: bookId })
+        .then(res.send(`Record with id:${bookId} --> deleted from DB!`))
+        .catch(err => {
             console.log(err);
-            return res.status(500).send();
-        }
-        res.send(`Record with id:${bookId} --> deleted from DB!`);
-    });
-};
+            res.status(500).send();
+        })
+}
